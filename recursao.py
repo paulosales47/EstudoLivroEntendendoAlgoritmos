87260d499dd6526f853a9b124b4842a10380d1e4
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2023-01-24 18:03:49 -0300

    Recursão - Somando itens de um array, contando a quantidade de itens e buscando maior valor

--- recursao.py
+++ recursao.py
@@ -13,8 +13,41 @@ def fatorial_tail(valor, total = 1):
     else:
         return fatorial_tail(valor - 1, valor * total)
 
+def somarItensArray(array):
+    if(len(array) == 0):
+        return 0
+    
+    return array.pop() + somarItensArray(array)
 
-print(fatorial(995))
-print(fatorial_tail(995))
+def contarItensArray(array):
+    
+    if(array == []):
+        return -1
+    
+    array.pop()
+    return 1 + contarItensArray(array)
+
+def buscarMaiorItemArray(array, maior_inicial = 0):
+    if(array == []):
+        return maior_inicial
+    
+    maior = array.pop(0)
+
+    if(maior > maior_inicial):
+        return buscarMaiorItemArray(array, maior)
+    else:
+        return buscarMaiorItemArray(array, maior_inicial)
 
 
+# print(fatorial(995))
+# print(fatorial_tail(995))
+
+# minha_lista = [0, 45, 5, 80]
+# print(somarItensArray(minha_lista))
+
+# minha_lista = list(range(101))
+# print(minha_lista)
+# print(contarItensArray(minha_lista))
+
+minha_lista = [0, 99, 120, 456, 12, 45000, 78, 99, 987, 125, 653, 753, 159, 1205, 996, 4502, 3598, 7894]
+print(buscarMaiorItemArray(minha_lista))
