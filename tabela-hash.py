f558af6a029cf3e3fcce4ef436c4be50e36f46c0
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2023-01-24 18:04:03 -0300

    Tabela Hash

---
+++ tabela-hash.py
@@ -0,0 +1,11 @@
+# caderno = dict()
+# caderno["maçã"] = 0.67
+# caderno["leite"] = 4.59
+# caderno["abacate"] = 1.78
+# print(caderno)
+# print(caderno["abacate"])
+
+lista_telefonica = {}
+lista_telefonica["aaa"] = 123
+lista_telefonica["aab"] = 789
+print(lista_telefonica.get("aac"))
