083d68ad17466bf8821e2f9ad1e66cc819377ec6
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2023-01-25 18:10:47 -0300

    Dijkstra

---
+++ dijkstra.py
@@ -0,0 +1,76 @@
+grafo = {}
+grafo["inicio"] = {}
+grafo["inicio"]["a"] = 6
+grafo["inicio"]["b"] = 2
+
+grafo["a"] = {}
+grafo["a"]["fim"] = 1
+
+grafo["b"] = {}
+grafo["b"]["a"] = 3
+grafo["b"]["fim"] = 5
+
+grafo["fim"] = {}
+
+infinito = float("inf")
+custos = {}
+custos["a"] = 6
+custos["b"] = 2
+custos["fim"] = infinito
+
+pais = {}
+pais["a"] = "inicio"
+pais["b"] = "inicio"
+pais["fim"] = None
+
+nodos_processados = []
+
+def procurarVerticeMenorCusto():
+    custo_mais_baixo = float("inf")
+    nodo_custo_mais_baixo = None
+
+    for nodo in custos:
+        custo = custos[nodo]
+        if custo < custo_mais_baixo and nodo not in nodos_processados:
+            custo_mais_baixo = custo
+            nodo_custo_mais_baixo = nodo
+    return nodo_custo_mais_baixo
+
+def buscarCaminhoMenorCusto():
+    nodo = procurarVerticeMenorCusto()
+    
+    while nodo is not None:
+        custo = custos[nodo]
+        vizinhos = grafo[nodo]
+        for n in vizinhos.keys():
+            novo_custo = custo + vizinhos[n]
+            if custos[n] > novo_custo:
+                custos[n] = novo_custo
+                pais[n] = nodo
+        nodos_processados.append(nodo)
+        nodo = procurarVerticeMenorCusto()
+
+
+
+
+print('--------------------------')
+print('ANTES')
+print('--------------------------')
+print(grafo.keys())
+print('--------------------------')
+print(pais.keys())
+print(pais.values())
+print('--------------------------')
+print(custos.keys())
+print(custos.values())
+buscarCaminhoMenorCusto()
+print('--------------------------')
+print('DEPOIS')
+print('--------------------------')
+print(grafo.keys())
+print('--------------------------')
+print(pais.keys())
+print(pais.values())
+print('--------------------------')
+print(custos.keys())
+print(custos.values())
